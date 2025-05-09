64f8069fcffc127cc8becedbd09832b49ed7f87c
ksyq12 <ksyq12@daum.net>
2025-05-09 19:36:26 +0900

    feat: 검색 리스트 셀 구현

---
+++ ShinyoungBookSearchApp/ShinyoungBookSearchApp/Presentation/Views/SearchResultCell.swift
@@ -0,0 +1,66 @@
+//
+//  SearchResultCell.swift
+//  ShinyoungBookSearchApp
+//
+//  Created by shinyoungkim on 5/9/25.
+//
+
+import UIKit
+import SnapKit
+
+final class SearchResultCell: UICollectionViewCell {
+    static let id = "SearchResultCell"
+    
+    private let bookTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .medium)
+        return label
+    }()
+    
+    private let authorLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let bookPriceLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        return label
+    }()
+    
+    private let bookInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        [
+            bookTitleLabel,
+            authorLabel,
+            bookPriceLabel
+        ].forEach { bookInfoStackView.addArrangedSubview($0) }
+        
+        [bookInfoStackView].forEach { addSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        bookInfoStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
