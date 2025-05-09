9b54265bc2275852277be4b93aed94c93ef68ff3
_GGDol <bohg6799@naver.com>
2025-05-09 11:31:27 +0900

    init: 프로젝트 세팅
    - 프로젝트 세팅

---
+++ AdvanceProject/AdvanceProject/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  AdvanceProject
+//
+//  Created by 양원식 on 5/9/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
