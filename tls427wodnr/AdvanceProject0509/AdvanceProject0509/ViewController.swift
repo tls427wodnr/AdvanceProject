314ad3759c1aea6f572729301290f41b57aba907
tls427wodnr <tlswodnr427@naver.com>
2025-05-09 10:58:11 +0900

    [Init]: 초기 프로젝트 세팅

---
+++ tls427wodnr/AdvanceProject0509/AdvanceProject0509/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvanceProject0509
+//
+//  Created by tlswo on 5/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
