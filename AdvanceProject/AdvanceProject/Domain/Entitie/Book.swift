cd7a2c22b1fe9db07f4eef27114a1d59fd0d8e56
_GGDol <bohg6799@naver.com>
2025-05-11 22:13:21 +0900

    feat: Book Entitie 구현

---
+++ AdvanceProject/AdvanceProject/Domain/Entitie/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  AdvanceProject
+//
+//  Created by 양원식 on 5/11/25.
+//
+
+struct Book {
+    let title: String
+    let thumbnail: String
+    let authors: [String]
+    let contents: String
+    let publisher: String
+    let url: String
+    let price: Int
+}
