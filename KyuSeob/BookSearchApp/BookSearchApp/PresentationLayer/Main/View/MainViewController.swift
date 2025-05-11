7a7683db96ab3367e9fc596ff8f047c644be4e4f
SongKyuSeob <sks5075@naver.com>
2025-05-12 02:51:43 +0900

    feat: 검색 결과 셀 탭 시 디테일뷰로 화면 전환 일부 구현

--- KyuSeob/BookSearchApp/BookSearchApp/PresentationLayer/Main/View/MainViewController.swift
+++ KyuSeob/BookSearchApp/BookSearchApp/PresentationLayer/Main/View/MainViewController.swift
@@ -11,12 +11,42 @@ import SnapKit
 import Then
 
 class MainViewController: UIViewController {
-    private let viewModel = MainViewModel()
+    private let viewModel: MainViewModel
     private let disposeBag = DisposeBag()
 
-    private let dummyButton = UIButton().then {
-        $0.setTitle("디테일 뷰", for: .normal)
-        $0.setTitleColor(.black, for: .normal)
+    private var searchResultBooks = [Book]()
+
+    private let searchBar = UISearchBar().then {
+        $0.placeholder = "책 제목을 입력해주세요"
+        $0.searchBarStyle = .minimal
+        $0.backgroundColor = .systemBackground
+    }
+
+    private lazy var resultCollectionView: UICollectionView = {
+        let layout = createCollectionViewLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(
+            SearchResultTableViewCell.self,
+            forCellWithReuseIdentifier: SearchResultTableViewCell.identifier
+        )
+        collectionView.register(
+            SectionHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: SectionHeaderView.identifier
+        )
+
+        return collectionView
+    }()
+
+    init(viewModel: MainViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
     }
 
     override func viewDidLoad() {
@@ -24,10 +54,6 @@ class MainViewController: UIViewController {
 
         configure()
     }
-}
-
-private extension MainViewController {
-
 }
 
 private extension MainViewController {
@@ -39,30 +65,117 @@ private extension MainViewController {
     }
 
     func setStyle() {
-
+        view.backgroundColor = .systemBackground // TODO: - 트러블슈팅 기록용
+        navigationController?.navigationBar.isHidden = true
     }
 
     func setHierarchy() {
-        view.addSubViews(views: dummyButton)
+        view.addSubviews(views: searchBar, resultCollectionView)
     }
 
     func setConstraints() {
-        dummyButton.snp.makeConstraints {
-            $0.width.equalTo(100)
-            $0.height.equalTo(40)
-            $0.center.equalToSuperview()
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(12)
+            $0.directionalHorizontalEdges.equalToSuperview().inset(12)
+            $0.height.equalTo(44)
+        }
+
+        resultCollectionView.snp.makeConstraints {
+            $0.directionalHorizontalEdges.equalToSuperview()
+            $0.top.equalTo(searchBar.snp.bottom).offset(10)
+            $0.bottom.equalToSuperview()
         }
     }
 
     func bind() {
-        dummyButton.rx.tap
-            .bind(to: viewModel.dummyButtonTapped)
-            .disposed(by: disposeBag)
-
-        viewModel.showDetailView
-            .subscribe(onNext: { [weak self] in
-                let detailViewController = BookDetailViewController()
-                self?.navigationController?.pushViewController(detailViewController, animated: true)
+        searchBar.rx.searchButtonClicked
+            .withLatestFrom(searchBar.rx.text.orEmpty)
+            .bind(onNext: { [weak self] query in
+                self?.viewModel.searchBooks(with: query)
+                self?.resultCollectionView.reloadData()
             }).disposed(by: disposeBag)
+
+        searchBar.rx.text.orEmpty
+            .bind(onNext: { [weak self] text in
+                if text.isEmpty {
+                    self?.searchResultBooks = []
+                    self?.resultCollectionView.reloadData()
+                }
+            }).disposed(by: disposeBag)
+
+        viewModel.searchResultBooks
+            .subscribe(onNext: { [weak self] books in
+                self?.searchResultBooks = books
+                self?.resultCollectionView.reloadData()
+            }).disposed(by: disposeBag)
+    }
+
+    func createCollectionViewLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(100)
+        )
+
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = .init(top: 10, leading: 20, bottom: 20, trailing: 20)
+
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(44)
+        )
+
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        section.boundarySupplementaryItems = [header]
+
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searchResultBooks.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: SearchResultTableViewCell.identifier,
+            for: indexPath
+        ) as? SearchResultTableViewCell else {
+            return UICollectionViewCell()
+        }
+
+        cell.configure(book: searchResultBooks[indexPath.item])
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeaderView.identifier, for: indexPath) as? SectionHeaderView else {
+            return UICollectionReusableView()
+        }
+        header.configure(with: "검색 결과")
+
+        return header
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailViewController = BookDetailViewController()
+        self.navigationController?.pushViewController(detailViewController, animated: true)
     }
 }
--- KyuSeob/BookSearchApp/BookSearchApp/PresentationLayer/Main/ViewModel/MainViewModel.swift
+++ KyuSeob/BookSearchApp/BookSearchApp/PresentationLayer/Main/ViewModel/MainViewModel.swift
@@ -10,20 +10,26 @@ import RxSwift
 import RxCocoa
 
 class MainViewModel {
+    private let searchBooksUseCase: SearchBooksUseCaseProtocol
 
-    let dummyButtonTapped = PublishRelay<Void>()
-    let showDetailView = PublishRelay<Void>()
+    let searchResultBooks = PublishRelay<[Book]>()
 
     private let disposeBag = DisposeBag()
 
-    init() {
-        bind()
+    init(searchBooksUseCase: SearchBooksUseCaseProtocol) {
+        self.searchBooksUseCase = searchBooksUseCase
     }
+}
 
-    private func bind() {
-        dummyButtonTapped
-            .bind(to: showDetailView)
-            .disposed(by: disposeBag)
+extension MainViewModel {
+    func searchBooks(with query: String) {
+        searchBooksUseCase.searchBooks(query: query)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onSuccess: { [weak self] result in
+                self?.searchResultBooks.accept(result)
+                print("검색 결과: \(result)")
+            }, onFailure: { error in
+                print("검색 에러: \(error)")
+            }).disposed(by: disposeBag)
     }
-
 }
