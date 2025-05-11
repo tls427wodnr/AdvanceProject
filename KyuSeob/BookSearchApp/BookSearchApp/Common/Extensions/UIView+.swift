4ffa55c6bc6823bf853aac117086207eac972573
SongKyuSeob <sks5075@naver.com>
2025-05-11 19:43:07 +0900

    feat: UIView extension 내에 여러 뷰 서브뷰로 추가하는 메서드 구현

---
+++ KyuSeob/BookSearchApp/BookSearchApp/Common/Extensions/UIView+.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+.swift
+//  BookSearchApp
+//
+//  Created by 송규섭 on 5/11/25.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubViews(views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+}
