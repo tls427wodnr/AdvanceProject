1350a32240f0a4f12dab11bf5c3c4472d854ca72
SongKyuSeob <sks5075@naver.com>
2025-05-15 15:57:14 +0900

    feat: 책의 고유 키 필요성에 따른 모델 일부 수정 및 관련 코드 반영

--- KyuSeob/BookSearchApp/BookSearchApp/DataLayer/CoreData/BookEntity.swift
+++ KyuSeob/BookSearchApp/BookSearchApp/DataLayer/CoreData/BookEntity.swift
@@ -10,19 +10,34 @@ import CoreData
 
 @objc(BookEntity)
 public class BookEntity: NSManagedObject {
+    @NSManaged public var isbn: String?
     @NSManaged public var title: String?
     @NSManaged public var authors: [String]?
     @NSManaged public var thumbnail: String?
     @NSManaged public var price: Int64
     @NSManaged public var contents: String?
+    @NSManaged public var cartBook: CartBookEntity?
+    @NSManaged public var recentBook: RecentBookEntity? // 문제의 코드?
 }
 
 extension BookEntity {
     func configure(from book: Book) {
+        self.isbn = book.isbn
         self.title = book.title
-        self.authors = book.authors
+        self.authors = book.authors 
         self.thumbnail = book.thumbnail
         self.price = Int64(book.price)
         self.contents = book.contents
+    }
+
+    func toDomain() -> Book {
+        return Book(
+            isbn: self.isbn ?? "",
+            title: self.title ?? "",
+            authors: self.authors ?? [],
+            thumbnail: self.thumbnail ?? "",
+            price: Int(self.price),
+            contents: self.contents ?? ""
+        )
     }
 }
